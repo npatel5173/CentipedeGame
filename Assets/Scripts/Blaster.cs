be078a587ed09b1dc2fc33b92fcd41152ccf5718
npatel5173 <nandini@email.uscupstate.edu>
2024-05-29 21:50:58 -0400

    Initial commit

---
+++ Assets/Scripts/Blaster.cs
@@ -0,0 +1,27 @@
+//date
+using UnityEngine;
+
+public class Blaster : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private Vector2 direction;
+    public float speed = 20f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        position += direction.normalized * speed * Time.fixedDeltaTime;
+        rigidbody.MovePosition(position);
+    }
+}
