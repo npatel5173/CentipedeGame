d08371916dafbba38f27e2cfb04a2263960d2391
npatel5173 <nandini@email.uscupstate.edu>
2024-06-06 21:16:23 -0400

    Mushroom Health

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Mushroom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Sprite[] states;
+    private SpriteRenderer spriteRenderer;
+    private int health;
+
+    private void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //health equals number of sprites
+        health = states.Length;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Damage(int amount){
+        //reduce health
+        health-=amount;
+
+        //check if there is still health remaining in the mushroom
+        if(health > 0){
+            //change the sprite on spriteRenderer with the index of the inverse of health
+            spriteRenderer.sprite = states[states.Length - health];
+        } else{
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        //check that dart collides with the mushroom
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Dart")){
+            //
+            Damage(1);
+        }
     }
 }
